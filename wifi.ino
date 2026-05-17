31ad1f86c8073f14b143d6fbc95282e1a9c897cc
Bruno <bruno@lmcursosdetransito.com.br>
2026-05-17 19:52:22 -0300

    funcionando, mas demora e o led apaga sozinho

--- wifi.ino
+++ wifi.ino
@@ -14,7 +14,6 @@ void setup() {
   server.begin();
 
   Serial.println("Servidor iniciado");
-  Serial.print("IP: ");
   Serial.println(WiFi.softAPIP());
 }
 
@@ -22,55 +21,47 @@ void loop() {
 
   WiFiClient client = server.available();
 
-  if (!client) {
-    return;
-  }
+  if (!client) return;
 
-  while (!client.available()) {
-    delay(1);
-  }
-
-  String request = client.readStringUntil('\r');
+  // 🔥 IMPORTANTE: lê linha correta HTTP
+  String request = client.readStringUntil('\n');
+  client.flush();
 
   Serial.println(request);
 
-  client.flush();
-
-  // Liga LED
+  // comandos para Arduino
   if (request.indexOf("/LED=ON") != -1) {
-
-    Serial.write('1');
+    Serial.println("ON");
   }
 
-  // Desliga LED
   if (request.indexOf("/LED=OFF") != -1) {
-
-    Serial.write('0');
+    Serial.println("OFF");
   }
 
-  // Página HTML
+  // 🔥 RESPOSTA HTTP CORRETA
   client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println("");
+  client.println("Content-Type: text/html; charset=UTF-8");
+  client.println("Connection: close");
+  client.println();
 
+  // HTML (SEU MESMO BOTÃO)
   client.println("<!DOCTYPE HTML>");
   client.println("<html>");
 
   client.println("<head>");
   client.println("<meta charset='UTF-8'>");
-  client.println("<meta name='viewport' content='width=device-width, initial-scale=1'>");
   client.println("<title>Controle LED</title>");
   client.println("</head>");
 
-  client.println("<body style='text-align:center;font-family:Arial;'>");
+  client.println("<body style='text-align:center;'>");
 
   client.println("<h1>LED Arduino Uno</h1>");
 
-  client.println("<button onclick=\"fetch('/LED=ON')\" style='width:200px;height:60px;font-size:20px;background:green;color:white;'>LIGAR</button>");
+  client.println("<button onclick=\"fetch('/LED=ON')\" style='width:200px;height:60px;font-size:20px;'>LIGAR</button>");
 
   client.println("<br><br>");
 
-  client.println("<button onclick=\"fetch('/LED=OFF')\" style='width:200px;height:60px;font-size:20px;background:red;color:white;'>DESLIGAR</button>");
+  client.println("<button onclick=\"fetch('/LED=OFF')\" style='width:200px;height:60px;font-size:20px;'>DESLIGAR</button>");
 
   client.println("</body>");
   client.println("</html>");
