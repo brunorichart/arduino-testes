d3f78cc594665b2d9954c603a81b02a463b48d3e
Bruno <bruno@lmcursosdetransito.com.br>
2026-05-17 19:32:37 -0300

    Código funcionando o botão, mas está apagando ainda

--- wifi.ino
+++ wifi.ino
@@ -5,8 +5,6 @@ const char* password = "12345678";
 
 WiFiServer server(80);
 
-bool ledState = false;
-
 void setup() {
 
   Serial.begin(115200);
@@ -15,9 +13,8 @@ void setup() {
 
   server.begin();
 
-  Serial.println();
   Serial.println("Servidor iniciado");
-  Serial.print("IP do ESP8266: ");
+  Serial.print("IP: ");
   Serial.println(WiFi.softAPIP());
 }
 
@@ -29,41 +26,36 @@ void loop() {
     return;
   }
 
-  Serial.println("Novo cliente");
-
-  // Espera dados
   while (!client.available()) {
     delay(1);
   }
 
-  // Lê requisição inteira
-  String request = client.readStringUntil('\n');
+  String request = client.readStringUntil('\r');
 
   Serial.println(request);
 
-  // Liga LED
-  if (request.indexOf("GET /LED=ON") >= 0) {
+  client.flush();
 
-    ledState = true;
+  // Liga LED
+  if (request.indexOf("/LED=ON") != -1) {
+
     Serial.write('1');
   }
 
   // Desliga LED
-  if (request.indexOf("GET /LED=OFF") >= 0) {
+  if (request.indexOf("/LED=OFF") != -1) {
 
-    ledState = false;
     Serial.write('0');
   }
 
-  // Resposta HTTP
+  // Página HTML
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
-  client.println("Connection: close");
-  client.println();
+  client.println("");
 
-  // HTML
   client.println("<!DOCTYPE HTML>");
   client.println("<html>");
+
   client.println("<head>");
   client.println("<meta charset='UTF-8'>");
   client.println("<meta name='viewport' content='width=device-width, initial-scale=1'>");
@@ -72,28 +64,13 @@ void loop() {
 
   client.println("<body style='text-align:center;font-family:Arial;'>");
 
-  client.println("<h1>Controle LED</h1>");
+  client.println("<h1>LED Arduino Uno</h1>");
 
-  // Estado
-  if (ledState) {
-    client.println("<h2 style='color:green;'>LED LIGADO</h2>");
-  } else {
-    client.println("<h2 style='color:red;'>LED DESLIGADO</h2>");
-  }
-
-  client.println("<br>");
-
-  // Botão ligar
-  client.println("<a href='/LED=ON'>");
-  client.println("<button style='width:200px;height:60px;font-size:20px;background:green;color:white;'>LIGAR</button>");
-  client.println("</a>");
+  client.println("<button onclick=\"fetch('/LED=ON')\" style='width:200px;height:60px;font-size:20px;background:green;color:white;'>LIGAR</button>");
 
   client.println("<br><br>");
 
-  // Botão desligar
-  client.println("<a href='/LED=OFF'>");
-  client.println("<button style='width:200px;height:60px;font-size:20px;background:red;color:white;'>DESLIGAR</button>");
-  client.println("</a>");
+  client.println("<button onclick=\"fetch('/LED=OFF')\" style='width:200px;height:60px;font-size:20px;background:red;color:white;'>DESLIGAR</button>");
 
   client.println("</body>");
   client.println("</html>");
@@ -101,6 +78,4 @@ void loop() {
   delay(1);
 
   client.stop();
-
-  Serial.println("Cliente desconectado");
 }
