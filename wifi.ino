466f1f35b7a0ab2fd5064e226646c8893eba2b80
Bruno <bruno@lmcursosdetransito.com.br>
2026-05-17 20:32:29 -0300

    Funciona e gerado pelo deepseek

--- wifi.ino
+++ wifi.ino
@@ -2,57 +2,47 @@
 
 const char* ssid = "ESP8266_WIFI";
 const char* password = "12345678";
-
 WiFiServer server(80);
 
 void setup() {
-
-  Serial.begin(115200);
-
+  Serial.begin(9600);  // IMPORTANTE: mesma velocidade do Arduino Uno
+  Serial.setTimeout(10);
+  
   WiFi.softAP(ssid, password);
-
   server.begin();
-
-  Serial.println(WiFi.softAPIP());
+  
+  Serial.println("ESP8266 pronto");
 }
 
 void loop() {
-
   WiFiClient client = server.available();
   if (!client) return;
-
+  
   String request = client.readStringUntil('\r');
   client.flush();
-
-  Serial.println(request);
-
-  // 🔥 ENVIA COMANDO LIMPO E FORTE
+  
+  // Envia comandos para o Arduino Uno com quebra de linha
   if (request.indexOf("/LED=ON") != -1) {
-    Serial.println("#ON");
+    Serial.println("ON");  // println adiciona \n automaticamente
+    Serial.println("<ON>");  // Para debug
   }
-
+  
   if (request.indexOf("/LED=OFF") != -1) {
-    Serial.println("#OFF");
+    Serial.println("OFF");  // println adiciona \n automaticamente
+    Serial.println("<OFF>");  // Para debug
   }
-
-  // resposta HTTP
+  
+  // HTML
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html; charset=UTF-8");
   client.println("Connection: close");
   client.println();
-
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
-  client.println("<head><meta charset='UTF-8'></head>");
+  client.println("<!DOCTYPE HTML><html>");
   client.println("<body style='text-align:center;'>");
-
   client.println("<h1>LED Arduino Uno</h1>");
-
   client.println("<button onclick=\"fetch('/LED=ON')\" style='width:200px;height:60px;background:green;color:white;'>LIGAR</button>");
   client.println("<br><br>");
   client.println("<button onclick=\"fetch('/LED=OFF')\" style='width:200px;height:60px;background:red;color:white;'>DESLIGAR</button>");
-
   client.println("</body></html>");
-
   client.stop();
 }
